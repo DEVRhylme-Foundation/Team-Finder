b18fe122b99f893e6df5c13bc55e9114aeccd9c8
Ghanshyam Singh <ghanshyam2005singh@gmail.com>
2025-04-28 00:17:07 +0530

    changed the whole project to REACT from Nextjs

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>welcome to team finder</h1>
+        <p>Find your perfect hackathon teammate!</p>
+        <button>Get Started</button>
+        </header>
+      <footer>
+        <p>© 2023 HackMate. All rights reserved.</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
